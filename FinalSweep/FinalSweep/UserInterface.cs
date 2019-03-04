40176662be1db2b75a43fcc99386806872afcd23
brad-johnson2 <46530051+brad-johnson2@users.noreply.github.com>
2019-03-04 14:34:26 -0600

    Update

---
+++ FinalSweep/FinalSweep/UserInterface.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalSweep
+{
+    static class UserInterface
+    {
+
+        static void MainMenu()
+        {
+            Console.WriteLine("What would you like to do? 1. Register a contestant");
+            string menuOption = Console.ReadLine();
+            switch (menuOption)
+            {
+                case "1":
+                    GetContestant();
+                    break;
+                default:
+                    MainMenu();
+                    break;
+            }
+
+
+        }
+
+        public Contestant GetContestant(Contestant contestant)
+        {
+            //Contestant contestant = new Contestant();
+
+
+            Console.WriteLine("First Name?");
+            contestant.firstName = Console.ReadLine();
+
+            Console.WriteLine("Last Name?");
+            contestant.lastName = Console.ReadLine();
+
+            Console.WriteLine("Email?");
+            contestant.emailAddress = Console.ReadLine();
+
+            contestant.regNumber = contestant.idNumber;
+            contestant.idNumber++;
+
+            return contestant;
+        }
+
+
+    }
+}
